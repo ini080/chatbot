8911be6f3b1fbe3354379d0e2101bf76e1fc5d29
ini080 <ini080@naver.com>
2019-07-23 16:21:43 +0900

    update

--- app.js
+++ app.js
@@ -43,10 +43,16 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 // get방식 요청일시.
-app.get('/', function(req, res, next){
+//초기 상태 get '시작'' 버튼으로 시작
+app.get('/keyboard', function(req, res){
+  const menu = {
+      "type": 'buttons',
+      "buttons": ["시작"]
+  };
 
-  res.send('Post로 요청하세요');
-
+  res.set({
+      'content-type': 'application/json'
+  }).send(JSON.stringify(menu));
 });
 
 // 카카오 API 요청은 무조건 POST 방식으로.
@@ -120,6 +126,193 @@ app.post('/dest/:desti', function(req, res){
     })
       setTimeout(function(){ res.json( {success:haveData?'데이터 있음' : '데이터 없음', message:Answer })   } , 1300);
 });
+
+
+// 카카오 API 요청은 무조건 POST 방식으로.
+app.post('/weather/:location', function(req, res){
+
+
+    // local 용
+    var location = req.params.location;
+    console.log('파라미터 : ' + req.params.location);
+
+
+    // 카톡용
+    var 특별시 = req.body.action.params.location;
+    var 동읍면 = req.body.action.params.dong;
+
+    /* 변수 선언 */
+    var nx = '';
+    var ny = '';
+    var 지역 = '';
+
+    // DB에서 location의 nx, ny 찾기.
+    ref.once('value',function(snapshot){
+      snapshot.forEach(function(childSnapshot){
+        var childKey = childSnapshot.key;
+        var childData = childSnapshot.val();
+
+        if( childData.Location_C == location ){
+          nx = childData.Location_NX;
+          ny = childData.Location_NY;
+
+          if( childData.Location_A != 'Null' ){
+              지역 += childData.Location_A;
+          }
+          if( childData.Location_B != 'Null' ){
+              지역 += childData.Location_B;
+          }
+
+          지역 += childData.Location_C;
+
+          console.log( 지역 + " " + nx + " " + ny )
+          var fullURL = $url + type_forecast;
+
+          var year = moment().format('YYYY');
+          var month = moment().format('MM');
+          var date =  moment().format('DD');
+          var hours = moment().format('HH');
+          var minutes =  moment().format('mm');
+
+          //fcstTime : 0000, 0300, 0600, 0900, 1200, 1500, 1800, 2100
+          var next_time = Number(hours);
+          if(next_time < 03) {
+            next_time = '0300';
+          }else if(next_time < 06){
+            next_time = '0600';
+          }else if(next_time < 09){
+            next_time = '0900';
+          }else if(next_time < 12){
+            next_time = '1200';
+          }else if(next_time < 15){
+            next_time = '1500';
+          }else if(next_time < 18){
+            next_time = '1800';
+          }else if(next_time < 21){
+            next_time = '2100';
+          }else if(next_time < 24){
+            next_time = '0000';
+          }
+
+
+          //Base_time  : 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300
+          var base_time = Number(hours);
+          if(base_time < 02) {
+            base_time = '2300';
+          }else if(base_time < 05){
+            base_time = '0200';
+          }else if(base_time < 08){
+            base_time = '0500';
+          }else if(base_time < 11){
+            base_time = '0800';
+          }else if(base_time < 14){
+            base_time = '1100';
+          }else if(base_time < 17){
+            base_time = '1400';
+          }else if(base_time < 20){
+            base_time = '1700';
+          }else if(base_time < 23){
+            base_time = '2000';
+          }
+
+          var nextDay = Number(year + month + date)+1;
+
+          fullURL += "?ServiceKey=" + $KEY;
+          fullURL += "&base_date=" + year + month + date;
+          fullURL += "&base_time=" + base_time ;
+          fullURL += "&nx=" + nx + "&ny=" + ny;
+          fullURL += "&pageNo=1&numOfRows=308";
+          fullURL += "&_type=json";
+
+          console.log(fullURL);
+
+          // API 요청
+          request(fullURL, function(rq_err,rq_res,rq_data){
+            $ = cheerio.load(rq_data);
+
+            obj = JSON.parse(rq_data);
+
+            var data_length = obj.response.body.totalCount;
+            console.log('데이터 길이 : ' + data_length)
+            console.log('베이스 타임 : ' + base_time)
+            console.log("다음시간 : " + next_time)
+
+            // 현재시간 이후부터 보여줌.
+            // 현재시간 이후 24시간만 알려줌
+            for( var i = 0; i < data_length; i++){
+              if( obj.response.body.items.item[i].fcstTime >= next_time || obj.response.body.items.item[i].fcstDate >= nextDay ){
+                  console.log(obj.response.body.items.item[i].fcstTime + " "+obj.response.body.items.item[i].fcstDate)
+              }
+            }
+          })
+
+        }
+      });
+  });
+
+
+  res.json({success:true , message: 특별시+동읍면})
+
+});
+
+
+//카톡 메시지 처리
+app.post('/translate/:text',function (req, res) {
+
+  var client_id = 'nPJYjRr1weJ4Hz4Cw5Rr';
+  var client_secret = 'V9fBY4Xy3f';
+
+
+  var api_url = 'https://openapi.naver.com/v1/language/translate';
+
+  var trans_text = req.body.action.params.text
+
+  console.log('번역 요청 : ' +  trans_text)
+
+  var options = {
+       url: api_url,
+       form: {'source':'ko', 'target':'en', 'text':trans_text},
+       headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+    };
+   request.post(options, function (error, response, body) {
+     if (!error && response.statusCode == 200) {
+       var objBody = JSON.parse(response.body);
+       //번역된 메시지
+       console.log('번역된 메시지 : ' + objBody.message.result.translatedText);
+         console.log(`요청 시간 => ${moment().format("YYYY-MM-DD HH:mm:ss")}`)
+       console.log('-------------------------------------------------')
+
+       //카톡으로 번역된 메시지를 전송하기 위한 메시지
+       let massage = {
+           "message": {
+               "text": objBody.message.result.translatedText
+           },
+       };
+       //카톡에 메시지 전송
+       res.set({
+           'content-type': 'application/json'
+       }).send(JSON.stringify(massage));
+
+
+     } else {
+       //네이버에서 메시지 에러 발생
+       res.status(response.statusCode).end();
+       console.log('error = ' + response.statusCode);
+
+       let massage = {
+           "message": {
+               "text": response.statusCode
+           },
+       };
+       //카톡에 메시지 전송 에러 메시지
+       res.set({
+           'content-type': 'application/json'
+       }).send(JSON.stringify(massage));
+     }
+   });
+
+});
+
 
 // port : 9000
 app.listen(9000, () => {
