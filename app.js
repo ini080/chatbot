f4469b37783aca16449332b62f42c2a97b5069ea
ini080@naver.com <ini080@naver.com>
2019-08-03 09:49:37 +0900

    코드수정

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ var ref = db.ref('/');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-
 app.get('/keyboard', function(req, res){
   const menu = {
       "type": 'buttons',
