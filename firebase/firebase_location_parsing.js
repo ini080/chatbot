3c8b16115619fd8957f31a3222d49aaf2036c27d
JeongGeon Heo <32252279+ini080@users.noreply.github.com>
2019-08-11 11:24:44 +0900

    delete Key

--- firebase/firebase_location_parsing.js
+++ firebase/firebase_location_parsing.js
@@ -10,13 +10,7 @@ let worksheet = workbook.Sheets["Sheet1"]
 
 // Firebase Info
 var config = {
-    apiKey: "AIzaSyD4ZkncsADsvtaU7D3H_wT7pKAWvNO-EWg",
-    authDomain: "kakao-location.firebaseapp.com",
-    databaseURL: "https://kakao-location.firebaseio.com",
-    projectId: "kakao-location",
-    storageBucket: "kakao-location.appspot.com",
-    messagingSenderId: "648358148479",
-    appId: "1:648358148479:web:bff412ab8ccb16c2"
+
 };
 
 firebase.initializeApp(config);
